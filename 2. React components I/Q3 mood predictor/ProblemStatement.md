29861a76787a3d8d807cf8b51b3ac681b70c31aa
Sahil pillania <sahilpillania88@gmail.com>
2023-04-09 12:30:26 +0530

    mood predictor program

---
+++ 2. React components I/Q3 mood predictor/ProblemStatement.md
@@ -0,0 +1,5 @@
+### Debug the React app to fix the binding issues
+
+Objectives -
+
+Clicking on the button should show the mood prediction.
---
+++ 2. React components I/Q3 mood predictor/src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./styles.css";
+
+const MOODS = [
+  "happy",
+  "sad",
+  "angry",
+  "excited",
+  "fearful",
+  "disgusted",
+  "surprised",
+];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      prediction: "",
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const moodIndex = Math.floor(Math.random() * MOODS.length);
+    this.setState({ prediction: MOODS[moodIndex] });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Mood Predictor</h1>
+        {this.state.prediction && (
+          <p>
+            Mood predictor has predicted that you mood will be
+            <b> {this.state.prediction} </b>today.
+          </p>
+        )}
+        <button onClick={this.handleClick}>Predict Mood</button>
+      </div>
+    );
+  }
+}
+
+export default App;
