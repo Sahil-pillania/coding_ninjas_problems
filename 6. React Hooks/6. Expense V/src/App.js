c31604306fd26a584895e9d02a971fa13febfc21
Sahil pillania <sahilpillania88@gmail.com>
2023-04-29 22:03:10 +0530

    Code

---
+++ 6. React Hooks/6. Expense V/src/App.js
@@ -0,0 +1,86 @@
+import { useState, useReducer } from "react";
+import ExpenseForm from "./components/ExpenseForm/ExpenseForm";
+import ExpenseInfo from "./components/ExpenseInfo/ExpenseInfo";
+import ExpenseList from "./components/ExpenseList/ExpenseList";
+import "./App.css";
+
+const reducer = (state, action) => {
+  const { payload } = action;
+  switch (action.type) {
+    case "ADD_EXPENSE": {
+      return {
+        expenses: [payload.expense, ...state.expenses]
+      };
+    }
+    case "REMOVE_EXPENSE": {
+      return {
+        expenses: state.expenses.filter((expense) => expense.id !== payload.id)
+      };
+    }
+    case "UPDATE_EXPENSE": {
+      const expensesDuplicate = state.expenses;
+      expensesDuplicate[payload.expensePos] = payload.expense;
+      return {
+        expenses: expensesDuplicate
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, { expenses: [] });
+  const [expenseToUpdate, setExpenseToUpdate] = useState(null);
+
+  const addExpense = (expense) => {
+    dispatch({ type: "ADD_EXPENSE", payload: { expense } });
+  };
+
+  const deleteExpense = (id) => {
+    dispatch({ type: "REMOVE_EXPENSE", payload: { id } });
+  };
+
+  const resetExpenseToUpdate = () => {
+    setExpenseToUpdate(null);
+  };
+
+  const updateExpense = (expense) => {
+    const expensePos = state.expenses
+      .map(function (exp) {
+        return exp.id;
+      })
+      .indexOf(expense.id);
+
+    if (expensePos === -1) {
+      return false;
+    }
+
+    dispatch({ type: "UPDATE_EXPENSE", payload: { expensePos, expense } });
+    return true;
+  };
+
+  return (
+    <>
+      <h2 className="mainHeading">Expense Tracker</h2>
+      <div className="App">
+        <ExpenseForm
+          addExpense={addExpense}
+          expenseToUpdate={expenseToUpdate}
+          updateExpense={updateExpense}
+          resetExpenseToUpdate={resetExpenseToUpdate}
+        />
+        <div className="expenseContainer">
+          <ExpenseInfo expenses={state.expenses} />
+          <ExpenseList
+            expenses={state.expenses}
+            deleteExpense={deleteExpense}
+            changeExpenseToUpdate={setExpenseToUpdate}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
