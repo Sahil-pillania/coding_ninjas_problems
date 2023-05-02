45396b9abe2221d57b3136a82c68638606dc23f6
Sahil pillania <sahilpillania88@gmail.com>
2023-05-02 21:08:12 +0530

    Code

---
+++ 7. FireBase/6. Updating the doc/src/App.js
@@ -0,0 +1,112 @@
+import { useState, useReducer } from "react";
+import "./App.css";
+
+// components imports
+import ExpenseForm from "./components/ExpenseForm/ExpenseForm";
+import ExpenseInfo from "./components/ExpenseInfo/ExpenseInfo";
+import ExpenseList from "./components/ExpenseList/ExpenseList";
+
+// react toasts
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+// import firebase methods here
+import { collection, addDoc, doc, updateDoc } from "firebase/firestore";
+import { db } from "./firebaseInit";
+
+const reducer = (state, action) => {
+  const { payload } = action;
+  switch (action.type) {
+    case "ADD_EXPENSE": {
+      return {
+        expenses: [payload.expense, ...state.expenses],
+      };
+    }
+    case "REMOVE_EXPENSE": {
+      return {
+        expenses: state.expenses.filter((expense) => expense.id !== payload.id),
+      };
+    }
+    case "UPDATE_EXPENSE": {
+      const expensesDuplicate = state.expenses;
+      expensesDuplicate[payload.expensePos] = payload.expense;
+      return {
+        expenses: expensesDuplicate,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, { expenses: [] });
+  const [expenseToUpdate, setExpenseToUpdate] = useState(null);
+
+  const addExpense = async (expense) => {
+    const expenseRef = collection(db, "expenses");
+    const docRef = await addDoc(expenseRef, expense);
+
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: { expense: { id: docRef.id, ...expense } },
+    });
+    toast.success("Expense added successfully.");
+  };
+
+  const deleteExpense = (id) => {
+    dispatch({ type: "REMOVE_EXPENSE", payload: { id } });
+  };
+
+  const resetExpenseToUpdate = () => {
+    setExpenseToUpdate(null);
+  };
+
+  const updateExpense = async (expense) => {
+    const expensePos = state.expenses
+      .map(function (exp) {
+        return exp.id;
+      })
+      .indexOf(expense.id);
+
+    if (expensePos === -1) {
+      return false;
+    }
+
+    // update expense in firestore here
+    const Ref = doc(db, "expenses", expense.id);
+
+    await updateDoc(Ref, {
+      text: expense.text,
+      amount: expense.amount,
+    });
+
+    dispatch({ type: "UPDATE_EXPENSE", payload: { expensePos, expense } });
+    toast.success("Expense updated successfully.");
+  };
+
+  return (
+    <>
+      <ToastContainer />
+      <h2 className="mainHeading">Expense Tracker</h2>
+      <div className="App">
+        <ExpenseForm
+          addExpense={addExpense}
+          expenseToUpdate={expenseToUpdate}
+          updateExpense={updateExpense}
+          resetExpenseToUpdate={resetExpenseToUpdate}
+        />
+        <div className="expenseContainer">
+          <ExpenseInfo expenses={state.expenses} />
+          <ExpenseList
+            expenses={state.expenses}
+            deleteExpense={deleteExpense}
+            changeExpenseToUpdate={setExpenseToUpdate}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ 7. FireBase/6. Updating the doc/src/ProblemStatement.md
@@ -0,0 +1,7 @@
+### Change the firebase method to update the expense in cloud firestore.
+
+Use the updateDoc method to update the expense in cloud firestore when user submits the update form.
+
+Learn more about the updateDoc method: <a href="https://firebase.google.com/docs/firestore/manage-data/add-data#update-data" target="_blank">here</a>
+
+Note: This is a follow-up question to the previous expense tracker project. So use the same firebase configuration in the solution code.
